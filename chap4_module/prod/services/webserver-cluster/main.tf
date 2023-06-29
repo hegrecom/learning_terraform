c5c013301dca36599867e21b790db0c2defe237f
Teekey <hegrecomm@gmail.com>
2023-06-29 20:27:20 +0900

    Add module

---
+++ chap4_module/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name = var.cluster_name
+
+  instance_type = "t3.nano"
+  min_size      = 2
+  max_size      = 3
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 3
+  desired_capacity      = 3
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 1
+  max_size              = 1
+  desired_capacity      = 1
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
